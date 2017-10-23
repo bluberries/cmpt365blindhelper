c57dbb237a46a1eefd411274d395e1fc639a9146
Ricky Ren <rgren@sfu.ca>
2017-10-22 21:57:34 -0700

    now takes png and mp4 files

--- application/Controller.java
+++ application/Controller.java
@@ -29,6 +29,7 @@ import utilities.Utilities;
 
 public class Controller {
 	
+	
 	@FXML
 	private ImageView imageView; // the image display window in the GUI
 	
@@ -80,8 +81,9 @@ public class Controller {
 		FileChooser fileChooser = new FileChooser();
 		File file = fileChooser.showOpenDialog(null);
 		String fileName = file.getAbsolutePath();
-		//System.out.println(fileName);
+		System.out.println(fileName);
 		return fileName;
+//		return "resources/test.mp4";
 	}
 	
 	protected void createFrameGrabber() throws InterruptedException {
@@ -112,7 +114,6 @@ public class Controller {
 		    		}
 		    	}
 			};
-			
 			slider.valueProperty().addListener(new InvalidationListener() {
 			    public void invalidated(Observable ov) {
 			       if (slider.isValueChanging()) {
@@ -134,14 +135,21 @@ public class Controller {
 			timer.scheduleAtFixedRate(frameGrabber, 0, Math.round(1000/framePerSecond), TimeUnit.MILLISECONDS);
 		  }
 	}
+
 	
 	@FXML
 	protected void openImage(ActionEvent event) throws InterruptedException {
 		// This method opens an image and display it using the GUI
 		// You should modify the logic so that it opens and displays a video
-		capture = new VideoCapture(getImageFilename()); // open video file
+		String fileName = getImageFilename();
+		capture = new VideoCapture(fileName); // open video file
 		if (capture.isOpened()) { // open successfully
+			System.out.println("Opened successfully");
 			createFrameGrabber();
+		}
+		else {
+			image = Imgcodecs.imread(fileName);
+			imageView.setImage(Utilities.mat2Image(image));
 		}
 		// You don't have to understand how mat2Image() works. 
 		// In short, it converts the image from the Mat format to the Image format
@@ -155,19 +163,22 @@ public class Controller {
 		// You should modify the logic so that it plays a video rather than an image
 		Mat frame = new Mat();
 		if (capture.read(frame)) {
+            synchronized (capture) {
+            	capture.notify();
+            }
 			// convert the image from RGB to grayscale
-			Mat grayFrame = new Mat();
-			Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+			Mat grayImage = new Mat();
+			Imgproc.cvtColor(frame, grayImage, Imgproc.COLOR_BGR2GRAY);
 			
-			// resize the frame
-			Mat resizedFrame = new Mat();
-			Imgproc.resize(grayFrame, resizedFrame, new Size(width, height));
+			// resize the image
+			Mat resizedImage = new Mat();
+			Imgproc.resize(grayImage, resizedImage, new Size(width, height));
 			
 			// quantization
-			double[][] roundedImage = new double[resizedFrame.rows()][resizedFrame.cols()];
-			for (int row = 0; row < resizedFrame.rows(); row++) {
-				for (int col = 0; col < resizedFrame.cols(); col++) {
-					roundedImage[row][col] = (double)Math.floor(resizedFrame.get(row, col)[0]/numberOfQuantizionLevels) / numberOfQuantizionLevels;
+			double[][] roundedImage = new double[resizedImage.rows()][resizedImage.cols()];
+			for (int row = 0; row < resizedImage.rows(); row++) {
+				for (int col = 0; col < resizedImage.cols(); col++) {
+					roundedImage[row][col] = (double)Math.floor(resizedImage.get(row, col)[0]/numberOfQuantizionLevels) / numberOfQuantizionLevels;
 				}
 			}
 			
@@ -200,12 +211,53 @@ public class Controller {
             sourceDataLine.write(click, 0, 20);
             sourceDataLine.drain();
             sourceDataLine.close();
-            synchronized (capture) {
-            	capture.notify();
-            }
 		} 
-//		else {
-//			// What should you do here?
-//		}
+		else if (image != null){
+			// convert the image from RGB to grayscale
+			Mat grayImage = new Mat();
+			Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
+			
+			// resize the image
+			Mat resizedImage = new Mat();
+			Imgproc.resize(grayImage, resizedImage, new Size(width, height));
+			
+			// quantization
+			double[][] roundedImage = new double[resizedImage.rows()][resizedImage.cols()];
+			for (int row = 0; row < resizedImage.rows(); row++) {
+				for (int col = 0; col < resizedImage.cols(); col++) {
+					roundedImage[row][col] = (double)Math.floor(resizedImage.get(row, col)[0]/numberOfQuantizionLevels) / numberOfQuantizionLevels;
+				}
+			}
+			
+			// I used an AudioFormat object and a SourceDataLine object to perform audio output. Feel free to try other options
+			AudioFormat audioFormat = new AudioFormat(sampleRate, sampleSizeInBits, numberOfChannels, true, true);
+			SourceDataLine sourceDataLine = AudioSystem.getSourceDataLine(audioFormat);
+			sourceDataLine.open(audioFormat, sampleRate);
+			sourceDataLine.start();
+			            
+			for (int col = 0; col < width; col++) {
+				byte[] audioBuffer = new byte[numberOfSamplesPerColumn];
+			    for (int t = 1; t <= numberOfSamplesPerColumn; t++) {
+			    	double signal = 0;
+			    	for (int row = 0; row < height; row++) {
+			    		int m = height - row - 1; // Be sure you understand why it is height rather width, and why we subtract 1 
+			    		int time = t + col * numberOfSamplesPerColumn;
+			    		double ss = Math.sin(2 * Math.PI * freq[m] * (double)time/sampleRate);
+			    		signal += roundedImage[row][col] * ss;
+			    	}
+			    	double normalizedSignal = signal / height; // signal: [-height, height];  normalizedSignal: [-1, 1]
+			    	audioBuffer[t-1] = (byte) (normalizedSignal*0x7F); // Be sure you understand what the weird number 0x7F is for
+			    }
+			    sourceDataLine.write(audioBuffer, 0, numberOfSamplesPerColumn);
+			}
+			byte[] click = new byte[numberOfSamplesPerColumn];
+			for(int i=0; i < 250; i++) {
+				click[2*i] = (byte) 0x56;
+				click[2*i+1] = (byte) 0x55;
+			}
+			sourceDataLine.write(click, 0, 20);
+			sourceDataLine.drain();
+			sourceDataLine.close();
+		}
 	} 
 }
